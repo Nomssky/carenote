ce05cb5daf9b6f04690dd3c0787450df25922066
kresna <kresna@users.noreply.github.com>
2026-06-30 14:02:10 +0700

    fix darkmode: slide-only anim, no opacity

--- components/AnimatedEntry.tsx
+++ components/AnimatedEntry.tsx
@@ -6,30 +6,23 @@ const EASE = Easing.out(Easing.cubic)
 
 export default function AnimatedEntry({ children }: { children: ReactNode }) {
   const navigation = useNavigation()
-  const opacity = useSharedValue(0)
-  const translateY = useSharedValue(12)
+  const translateY = useSharedValue(16)
 
   useEffect(() => {
     if (navigation.isFocused()) {
-      opacity.value = withTiming(1, { duration: 300, easing: EASE })
       translateY.value = withTiming(0, { duration: 300, easing: EASE })
     }
 
     const unsub = navigation.addListener('focus', () => {
-      opacity.value = withSequence(
-        withTiming(0, { duration: 60 }),
-        withTiming(1, { duration: 300, easing: EASE })
-      )
       translateY.value = withSequence(
-        withTiming(12, { duration: 60 }),
-        withTiming(0, { duration: 300, easing: EASE })
+        withTiming(12, { duration: 80 }),
+        withTiming(0, { duration: 280, easing: EASE })
       )
     })
     return unsub
   }, [navigation])
 
   const animStyle = useAnimatedStyle(() => ({
-    opacity: opacity.value,
     transform: [{ translateY: translateY.value }],
   }))
 
